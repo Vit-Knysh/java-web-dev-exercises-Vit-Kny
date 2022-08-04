43951308b467bacf98f85ba2c13bd277f4979f46
Vit-Knysh <v.knysh85@gmail.com>
2022-08-03 21:50:36 -0400

    Add info from external file

--- src/org/launchcode/java/studios/countingCharacters/Main.java
+++ src/org/launchcode/java/studios/countingCharacters/Main.java
@@ -1,19 +1,38 @@
 package org.launchcode.java.studios.countingCharacters;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        //Scanner input = new Scanner(System.in);
 
         //String phrase = "The features will be the same, but the implementation changes. Unit tests help in this scenario in that, changes to implementation should not require changes to outcome. Thus, if your tests continue to pass after the refactor, you can move on, knowing you have not inadvertently introduced a bug. When you write tests once, they provide a code lifetime’s worth of benefits.";
 
-        System.out.println("Enter a phrase: ");
-        String phrase = input.nextLine();
-        input.close();
+        //System.out.println("Enter a phrase: ");
+        //String phrase = input.nextLine();
+        //input.close();
+       // FileReader file = new FileReader("C:\\Users\\vknys\\OneDrive\\Documents\\English lessons.txt");
 
-CountingCharacters.CountChars(phrase);
+        try {
+            File myFile = new File("C:\\\\Users\\\\vknys\\\\OneDrive\\\\Documents\\\\English lessons.txt");
+            Scanner reader = new Scanner(myFile);
+            String myString = "";
+            while (reader.hasNextLine()) {
+                myString += reader.nextLine();
+                //System.out.println(myString);
+            }
+            CountingCharacters.CountChars(myString);
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occured.");
+            e.printStackTrace();
+        }
+
+//CountingCharacters.CountChars(file);
 
     }
 }
